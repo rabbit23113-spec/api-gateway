ca67ab15a97b532d70412c7119b464cbc117d1fc
Raffinnaisse <rabbit23113@gmail.com>
2026-07-15 15:59:46 +0300

    refactor: replace interface by any

--- internal/adapters/service.go
+++ internal/adapters/service.go
@@ -8,7 +8,7 @@ type Service struct {
 }
 
 func (s *Service) ValidateJWT(token string) error {
-	_, err := jwt.ParseWithClaims(token, &jwt.StandardClaims{}, func(t *jwt.Token) (interface{}, error) {
+	_, err := jwt.ParseWithClaims(token, &jwt.StandardClaims{}, func(t *jwt.Token) (any, error) {
 		return []byte("secret"), nil
 	})
 	if err != nil {
