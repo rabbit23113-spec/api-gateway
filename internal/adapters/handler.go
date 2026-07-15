786780c7066945f9298b7eb8f1f5ebd12acad9da
Raffinnaisse <rabbit23113@gmail.com>
2026-07-15 16:09:07 +0300

    feat: users route

--- internal/adapters/handler.go
+++ internal/adapters/handler.go
@@ -1,7 +1,9 @@
 package adapters
 
 import (
+	"fmt"
 	"main/internal/ports"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,6 +19,14 @@ func (h *Handler) InitRoutes() *gin.Engine {
 	api := router.Group("/api")
 	{
 		api.GET("/", func(ctx *gin.Context) { ctx.JSON(200, "ok") })
+		api.GET("/users", func(ctx *gin.Context) {
+			token := ctx.GetHeader("Authorization")
+			if err := h.Service.ValidateJWT(token); err != nil {
+				ctx.JSON(http.StatusUnauthorized, gin.H{"Error": err.Error()})
+				return
+			}
+			ctx.Redirect(http.StatusPermanentRedirect, fmt.Sprintf("http://localhost:11090/%s", ctx.Request.URL))
+		})
 	}
 
 	return router
